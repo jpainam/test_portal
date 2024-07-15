3df807e4764d652accb0e58742d12483f1e96b6a
System Administrator <root@Jean-Pauls-MBP.attlocal.net>
2024-07-15 19:00:17 -0400

    adding new password

--- public/js/clients.js
+++ public/js/clients.js
@@ -1,175 +1,199 @@
+var idfrais;
+
 $(document).ready(function () {
-    $("#comboClasses").change(chargerDonnees);
+    $("#fraisTable").DataTable({
+        "bInfo": false,
+        "scrollY": $(".page").height() - 100,
+        "searching": false,
+        "paging": false,
+        "columns": [
+            null,
+            {"width": "15%"},
+            {"width": "20%"},
+            {"width": "10%"}
+        ]
+    });
+
+    $("#comboClasses").change(chargerFrais);
+    //Popup form
+    $("#frais-dialog-form").dialog({
+        autoOpen: false,
+        height: 260,
+        width: 375,
+        modal: true,
+        resizable: false,
+        buttons: {
+            "Ajouter": function () {
+                ajoutFrais();
+                $(this).dialog("close");
+            },
+            Annuler: function () {
+                $(this).dialog("close");
+            }
+        }
+    });
+    $("#editfrais-dialog-form").dialog({
+        autoOpen: false,
+        height: 250,
+        width: 350,
+        modal: true,
+        resizable: false,
+        buttons: {
+            "Modifier": function () {
+                editFrais();
+                $(this).dialog("close");
+            },
+            Annuler: function () {
+                $(this).dialog("close");
+            }
+        }
+    });
+    $("#echeances, #editecheances").datepicker({
+        dateFormat: 'dd/mm/yy',
+        changeMonth: true,
+        changeYear: true,
+        prevText: '<i class="fa fa-chevron-left"></i>',
+        nextText: '<i class="fa fa-chevron-right"></i>'
+    });
+
+    //Bouton pour le popup
+    $("#img-ajout").on("click", function () {
+        removeRequiredFields([$("#comboClasses")]);
+        if ($("#comboClasses").val() === "") {
+            alertWebix(__t("Veuillez choisir d'abord une classe"));
+            addRequiredFields([$("#comboClasses")]);
+            return;
+        }
+        $("input[name=description]").val("");
+        $("input[name=montant]").val("");
+        $("#frais-dialog-form").dialog("open");
+    });
+
 
 });
 
-chargerDonnees = function () {
-    if ($("select[name=comboClasses]").val() === "") {
+function ajoutFrais() {
+    removeRequiredFields([$("#comboClasses")]);
+    if ($("#comboClasses").val() === 0 || $("#comboClasses").val() === "") {
+        alertWebix(__t("Veuillez choisir d'abord une classe"));
+        addRequiredFields([$("#comboClasses")]);
         return;
     }
     $.ajax({
-        url: "./classe/ajaxclasse",
+        url: "./ajax/ajouter",
         type: "POST",
         dataType: "json",
         data: {
-            "idclasse": $("select[name=comboClasses]").val()
+            "idclasse": $("#comboClasses").val(),
+            "description": $("input[name=description]").val(),
+            "montant": $("input[name=montant]").val(),
+            "echeances": $("input[name=echeances]").val(),
+            obligatoire: $("input[name=obligatoire]").prop('checked'),
+             codefrais: $("select[name=codefrais]").val()
         },
         success: function (result) {
-            $("#onglet1").html(result[0]);
-            $("#onglet2").html(result[1]);
-            $("#onglet3").html(result[2]);
-            $("#onglet4").html(result[9]);
-            $("#onglet6").html(result[3]);
-            $("#prof-principal").html(result[4]);
-            $("#cpe-principal").html(result[5]);
-            $("#resp-admin").html(result[6]);
-            $("#effectif").html(result[7]);
-            $("#total-frais").html(result[8]);
-            //$("#onglet5").html(result[10]);
-            $("#nouveauxeleves").html(result[11]);
-            $("#onglet5").html(result[12]);
-			
+            $("#frais-content").html(result[0]);
         },
         error: function (xhr, status, error) {
-            console.log(xhr.responseText);
+            alert(xhr.responseText);
+        }
+    });
+}
+function supprimerFrais(_id) {
+    $.ajax({
+        url: "./ajax/supprimer",
+        type: "POST",
+        dataType: "json",
+        data: {
+            "idfrais": _id,
+            "idclasse": $("#comboClasses").val()
+        },
+        success: function (result) {
+            $("#frais-content").html(result[0]);
+        },
+        error: function (xhr, status, error) {
+            alert(xhr.responseText);
+        }
+    });
+}
+
+/**
+ * Charge la liste de scolarite pour une classe donnne dans la datatable
+ * @returns {undefined}
+ */
+chargerFrais = function () {
+    $.ajax({
+        url: "./ajax/charger",
+        type: "POST",
+        dataType: "json",
+        data: {
+            "idclasse": $("#comboClasses").val()
+        },
+        success: function (result) {
+            $("#frais-content").html(result[0]);
+        },
+        error: function (xhr, status, error) {
+            alert(xhr.responseText);
         }
     });
 };
 
-function imprimer() {
-    if ($("select[name=code_impression]").val() === "") {
-        return;
-    }
-    removeRequiredFields([$("select[name=comboClasses]")]);
-    if ($("select[name=comboClasses]").val() === "") {
-        alertWebix(__t("Veuillez d'abord choisir une classe"));
-        addRequiredFields([$("select[name=comboClasses]")]);
-        return;
-    }
-    var frm = $("<form>", {
-        action: "./classe/imprimer",
-        target: "_blank",
-        method: "post"
-    }).append($("<input>", {
-        name: "code",
-        type: "hidden",
-        value: $("select[name=code_impression]").val()
-    })).append($("<input>", {
-        name: "type_impression",
-        type: "hidden",
-        value: $("input[name=type_impression]:checked").val()
-    })).append($("<input>", {
-        name: "idclasse",
-        type: "hidden",
-        value: $("select[name=comboClasses]").val()
-    })).appendTo("body");
-
-
-    var frais = $(".idsfrais:checked").map(function () {
-        return this.value;
-    }).get();
-
-    console.log(frais);
-
-    if ($("select[name=code_impression]").val() === "0003") {
-        frm.append($("<input>", {
-            name: "frais",
-            type: "hidden",
-            value: JSON.stringify(frais)
-        }));
-    }
-    frm.submit();
+function openEditForm(_id) {
+    /**
+     * Copie des element precemdent ecrit dans le formulaire
+     */
+    $("input[name=editdescription]").val($("input[name=description" + _id + "]").val());
+    $("input[name=editmontant]").val($("input[name=montant" + _id + "]").val());
+    $("input[name=editecheances]").val($("input[name=echeances" + _id + "]").val());
+    //console.log($("input[name=obligatoire" + _id + "]").val());
+    //console.log(($("input[name=obligatoire" + _id + "]").val() == 1));
+    $("input[name=editobligatoire]").prop('checked', ($("input[name=obligatoire" + _id + "]").val() == 1));
+    $("#editfrais-dialog-form").dialog("open");
+    idfrais = _id;
 }
-
-function envoyerRappel() {
-    if ($("select[name=comboClasses]").val() === "") {
-        return;
-    }
+/**
+ * Function utilser lorsqu'on clique sur Ajouter du formulaire
+ * edit frais
+ */
+function editFrais() {
     $.ajax({
-        url: "./classe/envoyerRappel",
+        url: "./ajax/edit",
         type: "POST",
         dataType: "json",
+        data: {
+            "idfrais": idfrais,
+            "idclasse": $("#comboClasses").val(),
+            "description": $("input[name=editdescription]").val(),
+            "montant": $("input[name=editmontant]").val(),
+            "echeances": $("input[name=editecheances]").val(),
+            codefrais: $("select[name=editcodefrais]").val(),
+            obligatoire: $("input[name=editobligatoire]").prop('checked')
+        },
+        success: function (result) {
+            $("#frais-content").html(result[0]);
+        },
+        error: function (xhr, status, error) {
+            alert(xhr.responseText);
+        }
+    });
+}
+
+function synchroniserDonnees(){
+    $.ajax({
+        url: "./synchroniser",
         data: {
             idclasse: $("select[name=comboClasses]").val()
         },
-        success: function (result) {
-            $("#onglet5").html(result[1]);
-            if(result[0]){
-                alertWebix(__t("Messages de rappel envoy&eacute; avec succ&egrave;s"));
-            }else{
-                alertWebix(__t("Erreur lors de l'envoi des messages de rappel"));
-            }
-        },
-        error: function (xhr, status, error) {
-            console.log(xhr.responseText);
-        }
-    });
-}
-function imprimerCompte(_ideleve) {
-
-    removeRequiredFields([$("select[name=comboClasses]")]);
-    if ($("select[name=comboClasses]").val() === "") {
-        alertWebix(__t("Veuillez d'abord choisir une classe"));
-        addRequiredFields([$("select[name=comboClasses]")]);
-        return;
-    }
-    var frm = $("<form>", {
-        action: "./eleve/imprimer",
-        target: "_blank",
-        method: "post"
-    }).append($("<input>", {
-        name: "code",
-        type: "hidden",
-        value: "0004"
-    })).append($("<input>", {
-        name: "ideleve",
-        type: "hidden",
-        value: _ideleve
-    })).appendTo("body");
-
-    frm.submit();
-
-}
-function synchroniserEmploisDuTemps(){
-    $.ajax({
-        url: "./classe/synchroniser",
-        type: "POST",
         dataType: "json",
-        data: {
-            idclasse: $("select[name=comboClasses]").val(),
-            action: "emploisdutemps"
-        },
-        success: function (result) {
-            $("#onglet3").html(result[0]);
-            if(result[0]){
-                alertWebix(__t("Emplois du temps synchroniser avec succ&egrave;s!!!"));
-            }else{
-                alertWebix(__t("Erreur de synchronisation"));
-            }
-        },
-        error: function (xhr, status, error) {
-            console.log(xhr.responseText);
-        }
-    });
-}
-function synchroniserManuels(){
-     $.ajax({
-        url: "./classe/synchroniser",
         type: "POST",
-        dataType: "json",
-        data: {
-            idclasse: $("select[name=comboClasses]").val(),
-            action: "manuelscolaires"
-        },
-        success: function (result) {
-            $("#onglet6").html(result[0]);
+        success: function(result){
             if(result[0]){
-                alertWebix(__t("Manuels scolaires synchronis&eacute;s avec succ&egrave;s!!!"));
+                alertWebix(__t("Synchronisation effectu&eacute;e avec succ&egrave;s"));
             }else{
-                alertWebix(__t("Erreur de synchronisation"));
+                console.log("Une erreur s'est produite");
             }
         },
-        error: function (xhr, status, error) {
+        error: function(xhr){
             console.log(xhr.responseText);
         }
     });
